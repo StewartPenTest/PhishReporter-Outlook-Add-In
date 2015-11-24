e5bf2677a2c8d79606f2e53b16e2e77faaf6e979
MSAdministrator <rickardj@missouri.edu>
2015-11-24 12:25:30 -0600

    Added GPL3 to all files
    Added licensing to all files

--- ForwardToAbuseAddin/Ribbon1.vb
+++ ForwardToAbuseAddin/Ribbon1.vb
@@ -1,4 +1,18 @@
-﻿Imports Microsoft.Office.Tools.Ribbon
+﻿'PhishReporter Outlook Add-In is an Outlook Add-In to Report Phishing emails to specific email addresses
+'Copyright (C) 2015  Josh Rickard
+'
+'This program is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License.
+'
+'This program is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+'You should have received a copy of the GNU General Public License
+'along with this program.  If not, see <http://www.gnu.org/licenses/>
+Imports Microsoft.Office.Tools.Ribbon
 
 
 Public Class HOME
